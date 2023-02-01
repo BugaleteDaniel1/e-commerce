c253fd3725f33cf85059b1bf15c33a0c1c215af0
BugaleteDaniel1 <106984939+BugaleteDaniel1@users.noreply.github.com>
2023-02-01 20:33:42 +0200

    .making the navbar

---
+++ src/assets/links.jsx
@@ -0,0 +1,29 @@
+import {
+  FaHome,
+  FaRegQuestionCircle,
+  FaBoxOpen,
+  FaCartPlus,
+} from "react-icons/fa";
+
+export const links = [
+  {
+    icon: <FaHome />,
+    name: "home",
+    url: "/",
+  },
+  {
+    icon: <FaRegQuestionCircle />,
+    name: "about",
+    url: "/about",
+  },
+  {
+    icon: <FaBoxOpen />,
+    name: "products",
+    url: "/products",
+  },
+  {
+    icon: <FaCartPlus />,
+    name: "cart",
+    url: "/cart",
+  },
+];
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,51 @@
 import { FaBars } from "react-icons/fa";
+import { useState, useEffect, useRef } from "react";
+import { links } from "../assets/links";
+import { Link } from "react-router-dom";
+import { nanoid } from "nanoid";
 
 export const Navbar = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const navbar = useRef(null);
+  const navContainer = useRef(null);
+
+  useEffect(() => {
+    const navHeight = navbar.current.getBoundingClientRect().height;
+    if (isNavbarOpen) {
+      navContainer.current.style.height = `${navHeight}px`;
+    } else {
+      navContainer.current.style.height = "0px";
+    }
+    console.log(navHeight);
+  }, [isNavbarOpen]);
+
+  const handleOpenNav = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
+  const navLinks = links.map((link) => {
+    return (
+      <li key={nanoid()}>
+        {link.icon}
+        <Link className="nav-a" to={link.url}>
+          {link.name}
+        </Link>
+      </li>
+    );
+  });
   return (
     <nav className="nav-bar">
       <div className="nav-header">
         <div className="nav-logo">Logo</div>
-        <div className="nav-hamburger">
+        <div onClick={handleOpenNav} className="nav-hamburger">
           <FaBars />
         </div>
       </div>
-      <ul className="nav-links">
-        <li>dadas</li>
-        <li>dadsa</li>
-        <li>dsadsa</li>
-      </ul>
+      <div ref={navContainer} className="nav-links-container">
+        <ul ref={navbar} className="nav-links">
+          {navLinks}
+        </ul>
+      </div>
     </nav>
   );
 };
