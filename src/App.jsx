8c48f29e6bad8b671191b68cd941af09bd1c43fe
BugaleteDaniel1 <106984939+BugaleteDaniel1@users.noreply.github.com>
2023-02-01 21:25:51 +0200

    small tweeks on navbar && added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Cart } from "./pages/Cart";
+import { Products } from "./pages/Products";
+import { About } from "./pages/About";
 
 const apiUrl = "https://course-api.com/react-store-products";
 
@@ -8,9 +13,15 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
   );
 }
 
